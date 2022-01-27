7793fd97a1f224106756f1a21740b47203463757
lemondrop81 <hacker814@hotmail.com>
2022-01-27 09:26:03 -0600

    Tried to pass variables to python script

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('calculator.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def foo():
+    bar = request.form['test']
+    print(bar)
 
 
 if __name__ == '__main__':
