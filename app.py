462b6ce4e8bdb0ad4e4b83341ef96ff99016f1b9
lemondrop81 <hacker814@hotmail.com>
2022-01-28 08:28:58 -0600

    Edit the calculator layout

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('calculator.html')
 
-@app.route('/calculator', methods=['GET', 'POST'])
-def foo():
-    bar = request.form['test']
-    print(bar)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
