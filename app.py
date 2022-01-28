a9c37090aa248a322d202148d49e08b982b10d9c
lemondrop81 <hacker814@hotmail.com>
2022-01-28 09:26:54 -0600

    can display which button is clicked

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('calculator.html')
+ButtonPressed = 0  
+@app.route('/calculator', methods=['GET', 'POST'])
+def calc():
+    if request.method == "POST":
+        if '1' in request.form:
+            return render_template("calculator.html", ButtonPressed = 1)
+        if '2' in request.form:
+            return render_template("calculator.html", ButtonPressed = 2)
+        if '3' in request.form:
+            return render_template("calculator.html", ButtonPressed = 3)
+        if '4' in request.form:
+            return render_template("calculator.html", ButtonPressed = 4)
+        if '5' in request.form:
+            return render_template("calculator.html", ButtonPressed = 5)
+        if '6' in request.form:
+            return render_template("calculator.html", ButtonPressed = 6)
+        if '7' in request.form:
+            return render_template("calculator.html", ButtonPressed = 7)
+        if '8' in request.form:
+            return render_template("calculator.html", ButtonPressed = 8)
+        if '9' in request.form:
+            return render_template("calculator.html", ButtonPressed = 9)
+        if '0' in request.form:
+            return render_template("calculator.html", ButtonPressed = 0)
 
+        return render_template("calculator.html", ButtonPressed = ButtonPressed)
+        # I think you want to increment, that case ButtonPressed will be plus 1.
+    return render_template("calculator.html", ButtonPressed = ButtonPressed)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
