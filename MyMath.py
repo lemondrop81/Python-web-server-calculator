90061e93e1fb8e847656cedc51ceea7deccff8be
lemondrop81 <hacker814@hotmail.com>
2022-02-03 09:21:11 -0600

    Update Mymath.py to be pep8 complient

--- MyMath.py
+++ MyMath.py
@@ -1,5 +1,6 @@
 import app
 
+
 class MyMath():
     """Contains math functions"""
 
@@ -11,8 +12,8 @@ class MyMath():
         """Returns the standard deviation of a list of numbers."""
         suma = 0
         for x in number:
-            suma += ((x - sum(number)/len(number))**2)
-        return (suma/(len(number)-1)) **0.5
+            suma += ((x - sum(number) / len(number)) ** 2)
+        return (suma/(len(number) - 1)) ** 0.5
 
     def largest(self, number):
         """Returns the largest number in the list."""
@@ -25,11 +26,12 @@ class MyMath():
         for num in number:
             sum += num
         avg = sum / len(number)
-        
         return avg
 
     def print(self, nums):
-        print(f"Standard deviation of nums: {math.standardDeviation(nums)}, the average of nums: {math.avg(nums)}")
+        print(f"Standard deviation of nums: " +
+              "{math.standardDeviation(nums)}, " + 
+              "the average of nums: {math.avg(nums)}")
 
 number = 0
 nums = []
